6146efd3d8f5362e16cb3af69456a0c238828707
kpiekut <karol.piekut@pm.me>
2023-06-27 21:46:13 +0100

    add game type selection

--- Scripts/welcome.js
+++ Scripts/welcome.js
@@ -12,30 +12,29 @@
 
 let playerOneName = document.querySelector('#p1name');
 let playerTwoName = document.querySelector('#p2name');
+let playGameButton = document.querySelector('#play');
 let gameSelectionTypeButtons = document.querySelectorAll("#buttons")
-let gameSelectionType;
-
+console.log(gameSelectionTypeButtons[0].firstChild.nextElementSibling);
 
 function gameSelection(e){
-    e.target.classList.toggle('active');
-    if (this.value === "pvp") {
-        gameSelectionType = 1;
-    } else if (this.value === "pvCPU"){
-        gameSelectionType = 2;
+    //e.target.classList.toggle('active');
+    let gameStateSelection = 0;
+    if (e.target.value === "pvp") {
+        gameStateSelection = 1;
+        e.target.classList.add('active');
+        gameSelectionTypeButtons[0].firstChild.nextElementSibling.nextElementSibling.classList.remove('active')
+    } else if (e.target.value === "pvCPU"){
+        gameStateSelection = 2;
+        e.target.classList.add('active');
+        gameSelectionTypeButtons[0].firstChild.nextElementSibling.classList.remove('active')
     }
-    console.log(gameSelectionType);
+
+    if (gameStateSelection === 1){
+        console.log("PVP")
+    } else if (gameStateSelection === 2){
+        console.log("PVCPU")
+    }
 }
-
-
-
-
-
-
-
-
-
-
-
 
 
 
