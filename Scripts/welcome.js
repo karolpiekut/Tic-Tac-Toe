b4e04eaf7a1e14180a47be43b2d5bac3ab24977f
kpiekut <karol.piekut@pm.me>
2023-06-28 21:28:19 +0100

    add username write to variable

--- Scripts/welcome.js
+++ Scripts/welcome.js
@@ -1,43 +1,49 @@
-//event listeners
 //query selectors
 //basic scripts
-//disable buttons
-//check length of the name
-//validate input
+
 //cpu greyed out
-//query selectors
 
-
-
-
-let playerOneName = document.querySelector('#p1name');
-let playerTwoName = document.querySelector('#p2name');
+let gameSelectionTypeButtons = document.querySelectorAll("#buttons");
 let playGameButton = document.querySelector('#play');
-let gameSelectionTypeButtons = document.querySelectorAll("#buttons")
-console.log(gameSelectionTypeButtons[0].firstChild.nextElementSibling);
+let p1Name = document.querySelector('#p1name');
+let p2Name = document.querySelector('#p2name');
+let p1NameValue;
+let p2NameValue;
+let gameStateSelection = 0;
+playGameButton.disabled = true;
+
 
 function gameSelection(e){
-    //e.target.classList.toggle('active');
-    let gameStateSelection = 0;
     if (e.target.value === "pvp") {
         gameStateSelection = 1;
         e.target.classList.add('active');
         gameSelectionTypeButtons[0].firstChild.nextElementSibling.nextElementSibling.classList.remove('active')
+        p2Name.value= "";
+        p2Name.removeAttribute("disabled");
+        playGameButton.disabled = false;
     } else if (e.target.value === "pvCPU"){
         gameStateSelection = 2;
         e.target.classList.add('active');
         gameSelectionTypeButtons[0].firstChild.nextElementSibling.classList.remove('active')
-    }
-
-    if (gameStateSelection === 1){
-        console.log("PVP")
-    } else if (gameStateSelection === 2){
-        console.log("PVCPU")
+        p2Name.value= "HAL";
+        p2Name.setAttribute("disabled", "");
+        playGameButton.disabled = false;
     }
 }
 
 
 
+function startGame(e){
+    e.preventDefault();
+    p1NameValue = p1Name.value;
+    p2NameValue = p2Name.value;
+    if (p1NameValue === undefined || p2NameValue === undefined ){
+        alert("Enter player name(s)");
+    }
+
+}
+
 Array.from(gameSelectionTypeButtons).forEach((button) => button.addEventListener('click', gameSelection));
+playGameButton.addEventListener('click', startGame);
 
 
